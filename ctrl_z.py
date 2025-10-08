0d99659725fab73081805f7a88c4703ce34e62e6
Rpinto30 <rodrigopinto3007@gmail.com>
2025-10-08 00:46:03 -0600

    mejor para el proyecto...

--- ctrl_z.py
+++ ctrl_z.py
@@ -1,1 +1,11 @@
-import keyboard
+op_editor = ['Escribir texto', 'insertar imagen', 'escalar imagen', 'mover imagen', 'cambiar color', 'pincel']
+
+while True:
+    print("1) Realizar acción\n2) CTRL+Z\n3) Consultar acciones realizadas\n4) Salir")
+    op = input("Selecciona una opción: ")
+    match op:
+        case '1':
+            for n,i in enumerate(op_editor,1):
+                print('\t',f"{n}) ", i)
+        case '4': break
+        case _: pass
