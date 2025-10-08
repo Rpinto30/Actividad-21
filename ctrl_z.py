34fb1406dd44b996aae3bbd6fdc2b637ac0e2ddd
Rpinto30 <rodrigopinto3007@gmail.com>
2025-10-08 01:09:52 -0600

    mejor para el proyecto...

--- ctrl_z.py
+++ ctrl_z.py
@@ -1,11 +1,34 @@
+from collections import deque
 op_editor = ['Escribir texto', 'insertar imagen', 'escalar imagen', 'mover imagen', 'cambiar color', 'pincel']
+events = deque()
 
 while True:
     print("1) Realizar acción\n2) CTRL+Z\n3) Consultar acciones realizadas\n4) Salir")
     op = input("Selecciona una opción: ")
     match op:
         case '1':
+            print('*'*50)
             for n,i in enumerate(op_editor,1):
                 print('\t',f"{n}) ", i)
+            op_e = input('Selecciona una opción: ')
+            try:
+                if 1<= int(op_e) <= len(op_editor):
+                    events.append(op_editor[int(op_e)-1])
+                    print(f'\nSe realizó: {op_editor[int(op_e)-1]}\n')
+                else: print("\nNinguna acción realizada\n")
+            except ValueError: print("\nEntrada no valida\n")
+        case '2':
+            print('*' * 50)
+            if events:
+                print(f"\nSe eliminó: {events[-1]}")
+                events.pop()
+            else: print("Sin acciones previas...")
+        case '3':
+            print('*'*50)
+            if events:
+                for n, i in enumerate(reversed(events), 1):
+                    print('\t', f"{n}) ", i)
+                print()
+            else: print("Sin acciones previas...")
         case '4': break
         case _: pass
