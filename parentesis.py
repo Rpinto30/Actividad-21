8f035354cdcca95b0c5cabfd67592ebc9b7623b1
Rpinto30 <rodrigopinto3007@gmail.com>
2025-10-07 23:42:02 -0600

    Pare

--- parentesis.py
+++ parentesis.py
@@ -1,8 +1,14 @@
-t = '(5+3)*(2-(1+4)))'
-queue_ = []
-for i in t:
-    if i == '(': queue_.append('(')
-    elif i == ')': queue_.pop()
-    print(queue_)
+t = input("Ingresa una operación matemática: ")
+def valid_expression(expression) -> bool:
+    expression = expression.strip()
+    try:
+        queue_ = []
+        for i in expression:
+            if i == '(': queue_.append('(')
+            elif i == ')': queue_.pop()
+            print(queue_)
+        if len(queue_) == 0: return True
+        else: return  False
+    except IndexError: return False
 
-print(queue_)
+print(valid_expression(t))
